677933e037ad3d24783538327590206f59171047
Bohdan Popovych <flecks07@gmail.com>
2022-02-18 18:00:13 +0200

    created CRUD for Route (without scaffolding)

--- app/controllers/railway_stations_controller.rb
+++ app/controllers/railway_stations_controller.rb
@@ -1,5 +1,5 @@
 class RailwayStationsController < ApplicationController
-  before_action :set_railway_station, only: %i[ show edit update destroy ]
+  before_action :set_railway_station, only: %i[show edit update destroy]
 
   # GET /railway_stations or /railway_stations.json
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_154212) do
+ActiveRecord::Schema.define(version: 2022_02_18_123631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "railway_stations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "routes", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
