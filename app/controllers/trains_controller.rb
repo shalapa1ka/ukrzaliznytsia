48812d7e90c7453c95fae876b0ac32a18b81c1e6
shalapa1ka <flecks07@gmail.com>
2022-02-22 18:48:36 +0200

    added footer added select of current_station and route for train added list of trains for routes added list of trains for stations changed column name in 'trains' table

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,70 +1,47 @@
 class TrainsController < ApplicationController
-  before_action :set_train, only: %i[ show edit update destroy ]
+  before_action :set_train, only: %i[show edit update destroy]
 
-  # GET /trains or /trains.json
   def index
     @trains = Train.all
   end
 
-  # GET /trains/1 or /trains/1.json
-  def show
-  end
+  def show; end
 
-  # GET /trains/new
   def new
     @train = Train.new
   end
 
-  # GET /trains/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /trains or /trains.json
   def create
     @train = Train.new(train_params)
 
-    respond_to do |format|
-      if @train.save
-        format.html { redirect_to train_url(@train), notice: "Train was successfully created." }
-        format.json { render :show, status: :created, location: @train }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @train.errors, status: :unprocessable_entity }
-      end
+    if @train.save
+      redirect_to @train
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /trains/1 or /trains/1.json
   def update
-    respond_to do |format|
-      if @train.update(train_params)
-        format.html { redirect_to train_url(@train), notice: "Train was successfully updated." }
-        format.json { render :show, status: :ok, location: @train }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @train.errors, status: :unprocessable_entity }
-      end
+    if @train.update(train_params)
+      redirect_to @train
+    else
+      render :new
     end
   end
 
-  # DELETE /trains/1 or /trains/1.json
   def destroy
-    @train.destroy
-
-    respond_to do |format|
-      format.html { redirect_to trains_url, notice: "Train was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to trains_url if @train.delete
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_train
-      @train = Train.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def train_params
-      params.require(:train).permit(:number)
-    end
+  def set_train
+    @train = Train.find(params[:id])
+  end
+
+  def train_params
+    params.require(:train).permit(:number, :route_id, :railway_station_id)
+  end
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,7 @@
 class Train < ApplicationRecord
   validates :number, presence: true
 
-  belongs_to :route
+  belongs_to :route, class_name: 'Route', foreign_key: :route_id
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :railway_station_id
+  has_many :tickets
 end
