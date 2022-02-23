9d3f0d8f9a8c44a4cfabd24b905dd06f7d56bf62
shalapa1ka <flecks07@gmail.com>
2022-02-23 17:49:11 +0200

    created carriages table added CRUD for carriages modified trains interface (added carriages info)

---
+++ app/controllers/carriages_controller.rb
@@ -0,0 +1,48 @@
+class CarriagesController < ApplicationController
+  before_action :set_carriage, only: %i[show edit update destroy]
+
+  def index
+    @carriage = Carriage.all
+  end
+
+  def show; end
+
+  def new
+    @carriage = Carriage.new
+  end
+
+  def edit; end
+
+  def create
+    @carriage = Carriage.new(carriage_params)
+
+    if @carriage.save
+      redirect_to @carriage
+    else
+      render :new
+    end
+
+  end
+
+  def update
+    if @carriage.update(carriage_params)
+      redirect_to @carriage
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    redirect_to carriages_url if @carriage.delete
+  end
+
+  private
+
+  def set_carriage
+    @carriage = Carriage.find(params[:id])
+  end
+
+  def carriage_params
+    params.require(:carriage).permit(:kind, :top_seats, :bottom_seats, :train_id)
+  end
+end
