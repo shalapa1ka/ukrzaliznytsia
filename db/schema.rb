dc93810d9f46d61dc5fa556f5e93f00d1711dafd
shalapa1ka <flecks07@gmail.com>
2022-03-24 22:47:14 +0200

    add new type of seats for carriages, station_index for railway_stations_routes and reverse boolean filed for train

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_105148) do
+ActiveRecord::Schema.define(version: 2022_03_21_144805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carriages", force: :cascade do |t|
-    t.integer "kind"
     t.integer "top_seats"
     t.integer "bottom_seats"
     t.bigint "train_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "side_bottom_seats"
+    t.integer "side_top_seats"
+    t.integer "number"
+    t.string "type"
+    t.integer "seating"
     t.index ["train_id"], name: "index_carriages_on_train_id"
   end
 
@@ -34,6 +38,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_105148) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "station_index"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -57,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_105148) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "railway_station_id"
     t.bigint "route_id"
+    t.boolean "reverse"
     t.index ["railway_station_id"], name: "index_trains_on_railway_station_id"
     t.index ["route_id"], name: "index_trains_on_route_id"
   end
