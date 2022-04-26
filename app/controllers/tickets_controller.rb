bd9dd9a49aa6ad6d348e07e666b6e5a1b8e3fb13
shalapa1ka <flecks07@gmail.com>
2022-04-26 11:26:29 +0300

    Do Devise authentication
    Divided app into users and admins. Divided app into users and admins.
    After a successful login, redirect regular users to the search page,
    and admins to the page that lists links to manage the application's
    resources. Moved admins views and controller to admin folder.
    Added admin flag to users. Added tickets list page: users can see
    owned tickets, admins - all tickets in the system. When buying a
    ticket, the ticket bound to the user who bought it (logged in).

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,37 +1,54 @@
 class TicketsController < ApplicationController
-  before_action :find_ticket, only: :show
-  before_action :find_train, only: :index
+  before_action :authenticate_user!, only: [:create, :index]
+  before_action :find_ticket, only: [:show, :destroy]
+  before_action :find_route, :find_from_station, :find_to_station, only: :new
 
   def index
-    @tickets = Ticket.where(train_id: @train.id)
+    if current_user.admin?
+      @tickets = Ticket.all
+    else
+      @tickets = Ticket.where(user: current_user)
+    end
   end
 
   def show; end
 
-  def buy
+  def new
     @ticket = Ticket.new
   end
 
   def create
-    @ticket = Ticket.new(tickets_params)
+    @ticket = current_user.tickets.new(tickets_params)
     if @ticket.save!
-      redirect_to train_tickets_path
+      redirect_to @ticket
     else
-      render :buy
+      render :new
     end
+  end
+
+  def destroy
+    redirect_to tickets_path, notice: 'Ticket successfully deleted!' if @ticket.destroy
   end
 
   private
 
   def tickets_params
-    params.require(:ticket).permit(:train_id, :name)
+    params.require(:ticket).permit(:train_id)
   end
 
   def find_ticket
     @ticket = Ticket.find(params[:id])
   end
 
-  def find_train
-    @train = Train.find(params[:train_id])
+  def find_route
+    @route = Route.find(params[:route_id])
+  end
+
+  def find_from_station
+    @from_station = RailwayStation.find params[:from]
+  end
+
+  def find_to_station
+    @to_station = RailwayStation.find params[:to]
   end
 end
