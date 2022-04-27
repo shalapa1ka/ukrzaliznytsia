3794d7f008decb90dce5581226964a6a96d9089e
shalapa1ka <flecks07@gmail.com>
2022-04-27 20:57:52 +0300

    Did stylizing
    Did stylizing of all pages and forms in app using Bootstrap.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,15 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'bootstrap'
+import '../../assets/stylesheets/application.scss'
+import Alert from 'bootstrap/js/dist/alert'
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
