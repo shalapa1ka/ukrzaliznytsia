9b51553f65cb37cfd9c5123dadba6a378be2191f
shalapa1ka <flecks07@gmail.com>
2022-04-28 12:48:34 +0300

    Correct some views
    Blocked accesses to admin panel for default users.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+  before_action :check_admin, only: :index
   def index
+  end
+
+  protected
+
+  def check_admin
+    redirect_to search_path, alert: "You can't enter to admin panel" unless current_user.admin?
   end
 end
